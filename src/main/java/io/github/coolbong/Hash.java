0c67feca37f0b74a035469d40eafa8e4ee821bc1
coolbong <coolbong.works@gmail.com>
2023-07-31 21:24:13 +0900

    add hash sha1, sha256, sha512

--- src/main/java/io/github/coolbong/Hash.java
+++ src/main/java/io/github/coolbong/Hash.java
@@ -2,6 +2,9 @@ package io.github.coolbong;
 
 import org.bouncycastle.crypto.Digest;
 import org.bouncycastle.crypto.digests.MD5Digest;
+import org.bouncycastle.crypto.digests.SHA1Digest;
+import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.crypto.digests.SHA512Digest;
 
 public class Hash {
 
@@ -18,6 +21,48 @@ public class Hash {
         digest.doFinal(buf, 0);
         return buf;
     }
+
+
+    public byte[] sha1(byte[] text) {
+        // create sha1 digest
+        Digest digest = new SHA1Digest();
+
+        //get block size for output buffer
+        int blockSize = digest.getDigestSize();
+        byte[] buf = new byte[blockSize];
+
+        digest.update(text, 0, text.length);
+        digest.doFinal(buf, 0);
+        return buf;
+    }
+
+
+    public byte[] sha256(byte[] text) {
+        // create sha256 digest
+        Digest digest = new SHA256Digest();
+
+        //get block size for output buffer
+        int blockSize = digest.getDigestSize();
+        byte[] buf = new byte[blockSize];
+
+        digest.update(text, 0, text.length);
+        digest.doFinal(buf, 0);
+        return buf;
+    }
+
+    public byte[] sha512(byte[] text) {
+        // create sha512 digest
+        Digest digest = new SHA512Digest();
+
+        //get block size for output buffer
+        int blockSize = digest.getDigestSize();
+        byte[] buf = new byte[blockSize];
+
+        digest.update(text, 0, text.length);
+        digest.doFinal(buf, 0);
+        return buf;
+    }
+
 
 
 }
--- src/test/java/io/github/coolbong/HashTest.java
+++ src/test/java/io/github/coolbong/HashTest.java
@@ -17,4 +17,30 @@ public class HashTest {
         byte[] ret = hash.md5(text);
         assertEquals("3E25960A79DBC69B674CD4EC67A72C62", toHex(ret));
     }
+
+    @Test
+    public void hash_sha1_test_001() {
+        byte[] text = "Hello world".getBytes(StandardCharsets.UTF_8);
+        Hash hash = new Hash();
+        byte[] ret = hash.sha1(text);
+        assertEquals("7B502C3A1F48C8609AE212CDFB639DEE39673F5E", toHex(ret));
+    }
+
+    @Test
+    public void hash_sha256_test_001() {
+        byte[] text = "Hello world".getBytes(StandardCharsets.UTF_8);
+        Hash hash = new Hash();
+        byte[] ret = hash.sha256(text);
+        assertEquals("64EC88CA00B268E5BA1A35678A1B5316D212F4F366B2477232534A8AECA37F3C", toHex(ret));
+    }
+
+    @Test
+    public void hash_sha512_test_001() {
+        byte[] text = "Hello world".getBytes(StandardCharsets.UTF_8);
+        Hash hash = new Hash();
+        byte[] ret = hash.sha512(text);
+        assertEquals("B7F783BAED8297F0DB917462184FF4F08E69C2D5E5F79A942600F9725F58CE1F" +
+                "29C18139BF80B06C0FFF2BDD34738452ECF40C488C22A7E3D80CDF6F9C1C0D47", toHex(ret));
+    }
+
 }
