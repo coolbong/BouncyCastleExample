89e9ed2be90baaa259db9e2f3bd5dadc0a54e275
coolbong <coolbong.works@gmail.com>
2023-08-03 17:52:57 +0900

    add des, aes mac

--- src/main/java/io/github/coolbong/Mac.java
+++ src/main/java/io/github/coolbong/Mac.java
@@ -1,12 +1,18 @@
 package io.github.coolbong;
 
+import org.bouncycastle.crypto.BlockCipher;
+import org.bouncycastle.crypto.BufferedBlockCipher;
+import org.bouncycastle.crypto.InvalidCipherTextException;
 import org.bouncycastle.crypto.digests.MD5Digest;
 import org.bouncycastle.crypto.digests.SHA1Digest;
 import org.bouncycastle.crypto.digests.SHA256Digest;
 import org.bouncycastle.crypto.engines.AESEngine;
-import org.bouncycastle.crypto.macs.CMac;
-import org.bouncycastle.crypto.macs.CMacWithIV;
-import org.bouncycastle.crypto.macs.HMac;
+import org.bouncycastle.crypto.engines.DESEngine;
+import org.bouncycastle.crypto.engines.DESedeEngine;
+import org.bouncycastle.crypto.macs.*;
+import org.bouncycastle.crypto.modes.CBCBlockCipher;
+import org.bouncycastle.crypto.paddings.ISO7816d4Padding;
+import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
 import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.crypto.params.ParametersWithIV;
 
@@ -55,6 +61,67 @@ public class Mac {
     public byte[] aesCmac128(byte[] key, byte[] text, byte[] iv) {
         CMac mac = new CMacWithIV(new AESEngine(), 128);
         mac.init(new ParametersWithIV(new KeyParameter(key), iv));
+        byte[] output = new byte[mac.getMacSize()];
+        mac.update(text, 0, text.length);
+        mac.doFinal(output, 0);
+        return output;
+    }
+
+    public byte[] aesMac(byte[] key, byte[] text, byte[] iv) {
+        CBCBlockCipherMac mac = new CBCBlockCipherMac(new AESEngine(), 128, null);
+
+        if (iv == null) {
+            mac.init(new KeyParameter(key));
+        } else {
+            mac.init(new ParametersWithIV(new KeyParameter(key), iv));
+        }
+
+        byte[] output = new byte[mac.getMacSize()];
+        mac.update(text, 0, text.length);
+        mac.doFinal(output, 0);
+
+        return output;
+    }
+
+    public byte[] desMac(byte[] key, byte[] text, byte[] iv) {
+        CBCBlockCipherMac mac = new CBCBlockCipherMac(new DESedeEngine(), 64, null);
+
+        if (iv == null) {
+            mac.init(new KeyParameter(key));
+        } else {
+            mac.init(new ParametersWithIV(new KeyParameter(key), iv));
+        }
+
+        byte[] output = new byte[mac.getMacSize()];
+        mac.update(text, 0, text.length);
+        mac.doFinal(output, 0);
+
+        return output;
+    }
+
+
+
+
+    public byte[] desMacMethod1Alg3(byte[] key, byte[] text, byte[] iv) {
+        ISO9797Alg3Mac mac = new ISO9797Alg3Mac(new DESEngine(), 64, null);
+        if (iv == null) {
+            mac.init(new KeyParameter(key));
+        } else {
+            mac.init(new ParametersWithIV(new KeyParameter(key), iv));
+        }
+        byte[] output = new byte[mac.getMacSize()];
+        mac.update(text, 0, text.length);
+        mac.doFinal(output, 0);
+        return output;
+    }
+
+    public byte[] desMacMethod2Alg3(byte[] key, byte[] text, byte[] iv) {
+        ISO9797Alg3Mac mac = new ISO9797Alg3Mac(new DESEngine(), 64, new ISO7816d4Padding());
+        if (iv == null) {
+            mac.init(new KeyParameter(key));
+        } else {
+            mac.init(new ParametersWithIV(new KeyParameter(key), iv));
+        }
         byte[] output = new byte[mac.getMacSize()];
         mac.update(text, 0, text.length);
         mac.doFinal(output, 0);
