db3d880b39d6c724eadd540b59d1b38d06b68986
coolbong <coolbong.works@gmail.com>
2023-08-10 20:50:39 +0900

    add paddings

--- src/main/java/io/github/coolbong/Padding.java
+++ src/main/java/io/github/coolbong/Padding.java
@@ -1,9 +1,6 @@
 package io.github.coolbong;
 
-import org.bouncycastle.crypto.paddings.BlockCipherPadding;
-import org.bouncycastle.crypto.paddings.ISO7816d4Padding;
-import org.bouncycastle.crypto.paddings.PKCS7Padding;
-import org.bouncycastle.crypto.paddings.ZeroBytePadding;
+import org.bouncycastle.crypto.paddings.*;
 
 import java.util.Arrays;
 
@@ -25,6 +22,36 @@ public class Padding {
         byte[] padded = Arrays.copyOf(input, input.length + length);
 
         BlockCipherPadding padding = new ISO7816d4Padding();
+        padding.init(null);
+        padding.addPadding(padded, input.length);
+        return padded;
+    }
+
+    public byte[] is1026dPadding(byte[] input, int blockSize) {
+        int length = blockSize - (input.length % blockSize);
+        byte[] padded = Arrays.copyOf(input, input.length + length);
+
+        BlockCipherPadding padding = new ISO10126d2Padding();
+        padding.init(null);
+        padding.addPadding(padded, input.length);
+        return padded;
+    }
+
+    public byte[] tbcPadding(byte[] input, int blockSize) {
+        int length = blockSize - (input.length % blockSize);
+        byte[] padded = Arrays.copyOf(input, input.length + length);
+
+        BlockCipherPadding padding = new TBCPadding();
+        padding.init(null);
+        padding.addPadding(padded, input.length);
+        return padded;
+    }
+
+    public byte[] x923Padding(byte[] input, int blockSize) {
+        int length = blockSize - (input.length % blockSize);
+        byte[] padded = Arrays.copyOf(input, input.length + length);
+
+        BlockCipherPadding padding = new X923Padding();
         padding.init(null);
         padding.addPadding(padded, input.length);
         return padded;
--- src/test/java/io/github/coolbong/PaddingTest.java
+++ src/test/java/io/github/coolbong/PaddingTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
+import static io.github.coolbong.Util.toHex;
+
 public class PaddingTest {
 
 
@@ -43,6 +45,65 @@ public class PaddingTest {
         Padding padding = new Padding();
         byte[] ret = padding.desPadding(data, 8);
 
+        Assert.assertArrayEquals(answer, ret);
+    }
+
+    @Test
+    public void test_padding_004() {
+        byte[] answer = {(byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x80, (byte)0x67, (byte)0x03 };
+        byte[] data = new byte[5];
+        Arrays.fill(data, (byte)0x5A);
+
+        Padding padding = new Padding();
+        byte[] ret = padding.is1026dPadding(data, 8);
+
+        System.out.println(toHex(ret));
+        //Assert.assertArrayEquals(answer, ret);
+    }
+
+
+    //
+    @Test
+    public void test_padding_005() {
+        byte[] answer = {(byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0xff, (byte)0xff, (byte)0xff };
+        byte[] data = new byte[5];
+        Arrays.fill(data, (byte)0x5A);
+
+        Padding padding = new Padding();
+        byte[] ret = padding.tbcPadding(data, 8);
+
+        //System.out.println(toHex(ret));
+        Assert.assertArrayEquals(answer, ret);
+    }
+
+
+    @Test
+    public void test_padding_006() {
+        byte[] answer = {(byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x00, (byte)0x00, (byte)0x03 };
+        byte[] data = new byte[5];
+        Arrays.fill(data, (byte)0x5A);
+
+        Padding padding = new Padding();
+        byte[] ret = padding.x923Padding(data, 8);
+
+        //System.out.println(toHex(ret));
+        Assert.assertArrayEquals(answer, ret);
+    }
+
+
+    @Test
+    public void test_padding_106() {
+        byte[] answer = {
+                (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A,
+                (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x08
+        };
+        byte[] data = new byte[8];
+        Arrays.fill(data, (byte)0x5A);
+
+        Padding padding = new Padding();
+        byte[] ret = padding.x923Padding(data, 8);
+
+        //System.out.println(toHex(ret));
         Assert.assertArrayEquals(answer, ret);
     }
 }
