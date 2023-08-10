9f1bdfab0f6cf9d1c7a5bd93026e8b8721e12c1e
coolbong <coolbong.works@gmail.com>
2023-08-10 18:28:40 +0900

    add 80 padding

--- src/main/java/io/github/coolbong/Padding.java
+++ src/main/java/io/github/coolbong/Padding.java
@@ -1,6 +1,7 @@
 package io.github.coolbong;
 
 import org.bouncycastle.crypto.paddings.BlockCipherPadding;
+import org.bouncycastle.crypto.paddings.ISO7816d4Padding;
 import org.bouncycastle.crypto.paddings.PKCS7Padding;
 import org.bouncycastle.crypto.paddings.ZeroBytePadding;
 
@@ -19,6 +20,16 @@ public class Padding {
         return padded;
     }
 
+    public byte[] desPadding(byte[] input, int blockSize) {
+        int length = blockSize - (input.length % blockSize);
+        byte[] padded = Arrays.copyOf(input, input.length + length);
+
+        BlockCipherPadding padding = new ISO7816d4Padding();
+        padding.init(null);
+        padding.addPadding(padded, input.length);
+        return padded;
+    }
+
     public byte[] pkcs7Padding(byte[] input, int blockSize) {
         int length = blockSize - (input.length % blockSize);
         byte[] padded = Arrays.copyOf(input, input.length + length);
@@ -29,9 +40,4 @@ public class Padding {
         return padded;
     }
 
-    public static void main(String[] args) {
-        Padding padding = new Padding();
-
-        byte[] ret = padding.zeroPadding(new byte[10], 16);
-    }
 }
--- src/test/java/io/github/coolbong/PaddingTest.java
+++ src/test/java/io/github/coolbong/PaddingTest.java
@@ -1,12 +1,9 @@
 package io.github.coolbong;
 
-import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
-
-import static io.github.coolbong.Util.toHex;
 
 public class PaddingTest {
 
@@ -14,8 +11,7 @@ public class PaddingTest {
 
     @Test
     public void test_padding_001() {
-
-        byte[] answer = {(byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, 0x00, 0x00, 0x00 };
+        byte[] answer = {(byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x00, (byte)0x00, (byte)0x00 };
         byte[] data = new byte[5];
         Arrays.fill(data, (byte)0x5A);
 
@@ -28,12 +24,24 @@ public class PaddingTest {
 
     @Test
     public void test_padding_002() {
-        byte[] answer = {(byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, 0x03, 0x03, 0x03 };
+        byte[] answer = {(byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x03, (byte)0x03, (byte)0x03 };
         byte[] data = new byte[5];
         Arrays.fill(data, (byte)0x5A);
 
         Padding padding = new Padding();
         byte[] ret = padding.pkcs7Padding(data, 8);
+
+        Assert.assertArrayEquals(answer, ret);
+    }
+
+    @Test
+    public void test_padding_003() {
+        byte[] answer = {(byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x5A, (byte)0x80, (byte)0x00, (byte)0x00 };
+        byte[] data = new byte[5];
+        Arrays.fill(data, (byte)0x5A);
+
+        Padding padding = new Padding();
+        byte[] ret = padding.desPadding(data, 8);
 
         Assert.assertArrayEquals(answer, ret);
     }
